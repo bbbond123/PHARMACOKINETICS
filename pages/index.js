303f685af9655fcfc0f375eb629b4e0126ad2da0
yibu.wang <>
2020-06-02 13:20:10 +0800

    inital

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchposts } from "../store/actions/postAction";
 
 export default function Home() {
+  const dispatch = useDispatch();
+
+  const { posts } = useSelector((state) => state.post);
+
+  useEffect(() => {
+    dispatch(fetchposts());
+  }, []);
+
   return (
     <div className="container">
       <Head>
@@ -12,40 +23,10 @@ export default function Home() {
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {posts &&
+          posts.map((item) => {
+            return <h1 key={item}>{item}</h1>;
+          })}
       </main>
 
       <footer>
@@ -54,7 +35,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -205,5 +186,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
